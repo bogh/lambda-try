c1adbabeb68413c6a6e03a913b4cf76cb8490b80
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2021-09-08 13:44:28 +0300

    Test

--- main.go
+++ main.go
@@ -5,13 +5,30 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+var (
+	DB *dynamodb.DynamoDB
 )
 
 func handler(ctx context.Context, event events.APIGatewayProxyRequest) (interface{}, error) {
-	return event, nil
+	// List all dynamo table
+	tablesOutput, err := DB.ListTables(&dynamodb.ListTablesInput{})
+	if err != nil {
+		return nil, err
+	}
+	return tablesOutput.TableNames, nil
 }
 
 func main() {
-	c := dynamo
+	sess, err := session.NewSession()
+	if err != nil {
+		panic(err)
+	}
+
+	DB = dynamodb.New(sess)
+
 	lambda.Start(handler)
 }
